a80a33e71123e2e34b8e8326b9f5d257a0f41e98
sunjunjie1314520 <248483600@qq.com>
2020-10-22 05:08:40 +0800

    2020/10/22-05:08:39

--- apps/example/urls.py
+++ apps/example/urls.py
@@ -6,7 +6,7 @@ from .models import User
 
 def Index(request):
     return JsonResponse({
-        'position': 'example MODULE 66666666666666',
+        'position': 'example MODULE Index',
     })
 
 urlpatterns = [
