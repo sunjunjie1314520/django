1d356c1d36f85f3e500b783932316402d88bf5c7
sunjunjie1314520 <248483600@qq.com>
2020-11-02 21:33:36 +0800

    2020/11/02-21:33:36

--- apps/scholl/admin.py
+++ apps/scholl/admin.py
@@ -1,3 +1,15 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Info, Examine
+
+
+@admin.register(Info)
+class InfoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'beian', 'xm', 'xb', 'xh', 'xy', 'zy', 'nj', 'create_time')
+    list_display_links = ['beian']
+
+@admin.register(Examine)
+class ExamineAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'opinion', 'info', 'create_time')
+    list_display_links = ['name']
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -8,6 +8,7 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'email', 'date_joined', 'modify_time')
     list_display_links = ['id', 'username']
 
+
 @admin.register(Users)
 class UsersAdmin(admin.ModelAdmin):
     list_display = ('id', 'phone', 'password', 'md5_password', 'create_time')
