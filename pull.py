1d3ff9ade84a8a3dd3bd2cabbe7d7d01c00c77bd
sunjunjie1314520 <248483600@qq.com>
2020-11-07 00:00:15 +0800

    2020/11/07-00:00:15

--- pull.py
+++ pull.py
@@ -14,9 +14,6 @@ def task():
         if r.status_code == 200:
             print(json.dumps(r.json(), sort_keys=True, indent=2, ensure_ascii=False))
             if r.json()['is_update']:
-                os.system('git reset --hard')
-                os.system('git pull')
-                os.system('python manage.py migrate')
                 data = {
                     'id': 3,
                     'is_update': False,
@@ -25,6 +22,12 @@ def task():
                 res = requests.post(baseURL + 'set_sync', data=data)
                 if res.status_code == 200:
                     print(json.dumps(res.json(), sort_keys=True, indent=2, ensure_ascii=False))
+
+                os.system('git reset --hard')
+                os.system('git pull')
+
+            if r.json()['is_migrate']:
+                os.system('python manage.py migrate')
 
         else:
             print(r.status_code)
