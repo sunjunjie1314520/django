afae258e41763f91674d4f6113b22f570c3eb219
sunjunjie1314520 <248483600@qq.com>
2020-11-06 23:25:33 +0800

    2020/11/06-23:25:33

--- utils/file_handle.py
+++ utils/file_handle.py
@@ -1,6 +1,10 @@
 def extension(name):
     str_len = len(name)
-    return name[str_len - 4: str_len]
+    new_name = name[str_len - 4: str_len]
+    if new_name in ['jpeg']:
+        return '.{0}'.format(new_name)
+    return new_name
 
 
-# print(file('7.jpg'))
+if __name__ == '__main__':
+    print(extension('123456.jpeg'))
