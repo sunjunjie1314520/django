35aeaeaa6cd7fbc75bacad105c94ea5451bde72e
sunjunjie1314520 <248483600@qq.com>
2020-11-06 20:52:34 +0800

    2020/11/06-20:52:34

--- apps/scholl/admin.py
+++ apps/scholl/admin.py
@@ -6,7 +6,7 @@ from .models import Info, Examine
 
 @admin.register(Info)
 class InfoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'beian', 'xm', 'xb', 'xh', 'xy', 'zy', 'nj', 'status', 'create_time')
+    list_display = ('id', 'beian', 'xm', 'xb', 'xh', 'xy', 'zy', 'nj', 'status', 'users', 'create_time')
     list_display_links = ['beian']
 
 @admin.register(Examine)
