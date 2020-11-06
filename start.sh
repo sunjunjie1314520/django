bcdc0711189580e516b79ffd1d1da24dbe51c513
sunjunjie1314520 <248483600@qq.com>
2020-11-06 17:48:05 +0800

    2020/11/06-17:48:05

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 ps -aux | grep python3|xargs kill -9
-nohup python3 /django/update/manage.py runserver 0.0.0.0:5001 > djo.out 2>&1 &
+nohup python3 /root/django/manage.py runserver 0.0.0.0:8082 > djo.out 2>&1 &
