b443f6dcd511e27d62d9b3a741dc66c3a1381020
sunjunjie1314520 <248483600@qq.com>
2020-11-13 21:34:23 +0800

    2020/11/13-21:34:22

--- apps/system/admin.py
+++ apps/system/admin.py
@@ -5,5 +5,5 @@ from .models import Config
 
 @admin.register(Config)
 class ConfigAdmin(admin.ModelAdmin):
-    list_display = ('id', 'money', 'create_time')
+    list_display = ('id', 'money', 'not_money', 'create_time')
     list_display_links = ['id']
